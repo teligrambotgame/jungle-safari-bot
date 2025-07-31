8b38bb28671a0ee7fd5b9aa05341e554ed62a6f1
teligrambotgame <technicalengineeringworks@gmail.com>
2025-07-31 15:02:44 +0530

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    ...
-    await update.message.reply_text("🦁 Welcome to Jungle Safari!", reply_markup=reply_markup)
+import telebot
+import os
+
+TOKEN = os.getenv("8486321938:AAGvAj1D6UBbJLkrxEMh29VNAriMMennGec")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "🌴 Welcome to Jungle Safari Bot! Let's begin your adventure!")
+
+bot.polling()
